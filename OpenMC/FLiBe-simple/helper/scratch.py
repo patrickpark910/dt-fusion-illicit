926ed9b0c03f6f0f6eb85aec0682ac2f1c097aef
Patrick Park <patrickpark910@gmail.com>
2025-07-07 06:37:50 -0400

    major fixes, cleanup, plots

--- OpenMC/FLiBe-simple/helper/scratch.py
+++ OpenMC/FLiBe-simple/helper/scratch.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 """
 Testing out helper functions
   Patrick 2025-06-25
@@ -7,10 +8,17 @@ import numpy as np
 
 # Nuclear constants
 #   from atom.kaeri.re.kr/nuchart/
+=======
+
+import numpy as np
+
+
+>>>>>>> Stashed changes
 AVO = 6.022141076e+23
 AMU_LI6, AMU_LI7 = 6.0150, 7.0160 # 6.01512288742, 7.01600343426 # amu = g/mol
 AMU_F19 = 18.9984 # 18.99840316207
 AMU_BE9 =  9.0120 # 9.012183062
+<<<<<<< Updated upstream
 
 def li_atfracs(w6):
     """
@@ -83,3 +91,61 @@ if __name__ == '__main__':
     print(flibe_ndens(mol_lif=0.66, mol_bef2=0.34,dens_flibe=1.80,enrich_li=0.20))
 
 
+=======
+AMU_U = 238.02891 # for natural enrichment
+AMU_UF4 = AMU_U + 4 * AMU_F19
+AMU_FLIBE = 98.89 # g/mol
+DENSITY_UF4 = 6.7 # g/cm3
+
+
+def calc_mix_vol_fracs(mtu, volume=342e6, density_flibe=1.94, displace=False):
+    """
+    Calculate the volume fractions of FLiBe and UF4. 
+    Assumes UF4 concentration small enough that its addition does NOT change FLiBe volume
+      after conversaion with J.L. Ball --ppark 2025-07-03
+
+    Args:
+        mtu : float : metric tons uranium
+        volume : float : cc of system
+        density_flibe : float : g/cm3 of FLiBe
+        displace : bool : default False, whether to deduct UF4 volume from FLiBe volume (if UF4 is assumed to/not to dissolve in FLiBe)
+
+    Returns:
+        (vf_flibe, vf_uf4) : 2-ple of floats : volume fractions
+    """
+    # Convert inputs to SI units
+    mass_u = mtu * 1e3
+    density_flibe = density_flibe * 1e3 # kg/m3
+    density_uf4   = DENSITY_UF4 * 1e3   # kg/m3  
+    volume = float(volume) / 1e6
+
+    # Compute volumes
+    mass_uf4 = mass_u * (AMU_UF4 / AMU_U)
+    vol_uf4 = mass_uf4 / density_uf4
+    
+    if displace:
+        vol_flibe = volume - vol_uf4
+    if not displace:
+        vol_flibe = volume
+
+    # Compute volume fractions
+    vf_flibe = vol_flibe/(vol_flibe+vol_uf4)
+    vf_uf4   = vol_uf4/(vol_flibe+vol_uf4)
+
+    # print(f"volumes: total {volume_m3} m3, flibe {vol_flibe_m3} m3, uf4 {vol_uf4_m3:.4f} m3 | deduct UF4 from FLiBe volume: {subtract}") # For debug
+
+    return vf_flibe, vf_uf4
+
+if __name__ == '__main__':
+    print(calc_mix_vol_fracs(50,displace=True))
+    print(calc_mix_vol_fracs(50,displace=False))
+
+    rho   = 6.7                     # g/cm³
+    M_UF4 = 238.0289 + 4*18.9984    # ≃314.0225 g/mol
+    N_A   = 6.02214076e23           # mol⁻¹
+    f_U   = 238.0289/M_UF4          # mass frac. of U in UF4
+    f238  = 1 - 0.7204/100          # weight frac. U238 (natural U235 = 0.7204 wt%)
+    n     = rho/M_UF4 * N_A * f_U * f238   # atoms U238 per cm³
+    n_bcm = n/1e24                          # atoms per barn-cm
+    print(n_bcm)  # → ≃0.0033651432
+>>>>>>> Stashed changes
