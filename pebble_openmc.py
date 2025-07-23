6b6cd7c7a2bcd6f0038d1130472ac80638de1139
Patrick Park <pp0191@princeton.edu>
2025-07-23 13:23:54 -0400

    HCPB fixed BISO specs, ready to run 1e7nps

--- pebble_openmc.py
+++ pebble_openmc.py
@@ -3,38 +3,42 @@ import os, sys
 import numpy as np
 
 # Import helper functions
-sys.path.insert(0, f"{os.getcwd()}/Python/")
-from parameters import *
-from utilities import *
+from Python.parameters import *
+from Python.utilities import *
 
 
 def main():
 
     print("\n\n")
     print("="*42)
-    print(f"Running Pebble OpenMC model for Li-6 enrichment: 60 wt%")
+    print(f"Running Helium-Cooled Pebble Bed OpenMC model for Li-6 enrichment: 60 wt%")
         
-    current_run = Pebble(enrich_li=60, u_list=MASS_U_LIST_PEBBLE)
+    current_run = HCPB(enrich_li=60, u_list=MASS_U_LIST_HCPB, run_openmc=False)
 
-    if os.path.isdir(current_run.path):
-        print(f"Warning. Directory {current_run.path} already exists, so running OpenMC will fail. Skipping...")
-        return
-    else:
-        current_run.set_xs_path()
-        current_run.run_openmc()
-
+    if current_run.run:
+        if os.path.isdir(current_run.path):
+            print(f"Warning. Directory {current_run.path} already exists, so running OpenMC will fail. Skipping...")
+        else:
+            current_run.run_openmc()
 
 
-class Pebble:
 
-    def __init__(self, enrich_li=60, u_list=MASS_U_LIST_PEBBLE):
+class HCPB:
+
+    def __init__(self, enrich_li=60, u_list=MASS_U_LIST_HCPB, temp_k=900, run_openmc=False):
 
         self.e = enrich_li
-        self.name = f'Pebble_Li{self.e}_7_22'
         self.u_list = u_list
+        self.temp = temp_k
+        self.run  = run_openmc
+        self.name = f'HCPB_FW4.3cm_Li{self.e}_NUO2_900K_2025-07-22'
         self.path = f"./OpenMC/{self.name}/"
 
-        # Eurofer
+        """ MATERIALS
+        Primary reference: P. Lu et al., "Activation analysis for the HCPB blanket module in the European DEMO", Fus. Engr. Des. (2017)
+        """
+
+        # Eurofer -- structural
         eurofer = openmc.Material(name='Eurofer')
         eurofer.set_density('g/cm3', 7.8)
         eurofer.add_element('Fe', 89.0026, percent_type='wo')
@@ -58,113 +62,108 @@ class Pebble:
         eurofer.add_element('Ta', 0.12, percent_type='wo')
         eurofer.add_element('W', 1.0987, percent_type='wo')
 
-        # Lithium ceramic
-        lithium_ceramic = openmc.Material(name='LithiumCeramic')
+        # Li4SiO4 -- lithium ceramic breeder
+        li4sio4 = openmc.Material(name='Li4SiO4')
         '''density from Activation calculations for multiple recycling of breeder ceramics by
         melt processing'''
-        lithium_ceramic.set_density('g/cm3', 2.42) 
-        lithium_ceramic.add_element('Li', 22.415, percent_type='wo', enrichment_target='Li6', enrichment_type='wo', enrichment=enrich_li)
-        lithium_ceramic.add_element('Si', 24.077, percent_type='wo')
-        lithium_ceramic.add_element('O', 53.39, percent_type='wo')
-        lithium_ceramic.add_element('Al', 0.003, percent_type='wo')
-        lithium_ceramic.add_element('C', 0.1, percent_type='wo')
-        lithium_ceramic.add_element('Ca', 0.003, percent_type='wo')
-        lithium_ceramic.add_element('Co', 0.0002, percent_type='wo')
-        lithium_ceramic.add_element('Cr', 0.0001, percent_type='wo')
-        lithium_ceramic.add_element('Cu', 0.0001, percent_type='wo')
-        lithium_ceramic.add_element('Fe', 0.0005, percent_type='wo')
-        lithium_ceramic.add_element('K', 0.0001, percent_type='wo')
-        lithium_ceramic.add_element('Mg', 0.0005, percent_type='wo')
-        lithium_ceramic.add_element('Mn', 0.0001, percent_type='wo')
-        lithium_ceramic.add_element('Pt', 0.009, percent_type='wo')
-        lithium_ceramic.add_element('Na', 0.002, percent_type='wo')
-        lithium_ceramic.add_element('Ni', 0.0002, percent_type='wo')
-        lithium_ceramic.add_element('Ti', 0.0005, percent_type='wo')
-        lithium_ceramic.add_element('Zn', 0.0002, percent_type='wo')
-        lithium_ceramic.add_element('Zr', 0.0001, percent_type='wo')
+        li4sio4.set_density('g/cm3', 2.42) 
+        li4sio4.add_element('Li', 22.415, percent_type='wo', enrichment_target='Li6', enrichment_type='wo', enrichment=enrich_li)
+        li4sio4.add_element('Si', 24.077, percent_type='wo')
+        li4sio4.add_element('O', 53.39, percent_type='wo')
+        li4sio4.add_element('Al', 0.003, percent_type='wo')
+        li4sio4.add_element('C', 0.1, percent_type='wo')
+        li4sio4.add_element('Ca', 0.003, percent_type='wo')
+        li4sio4.add_element('Co', 0.0002, percent_type='wo')
+        li4sio4.add_element('Cr', 0.0001, percent_type='wo')
+        li4sio4.add_element('Cu', 0.0001, percent_type='wo')
+        li4sio4.add_element('Fe', 0.0005, percent_type='wo')
+        li4sio4.add_element('K', 0.0001, percent_type='wo')
+        li4sio4.add_element('Mg', 0.0005, percent_type='wo')
+        li4sio4.add_element('Mn', 0.0001, percent_type='wo')
+        li4sio4.add_element('Pt', 0.009, percent_type='wo')
+        li4sio4.add_element('Na', 0.002, percent_type='wo')
+        li4sio4.add_element('Ni', 0.0002, percent_type='wo')
+        li4sio4.add_element('Ti', 0.0005, percent_type='wo')
+        li4sio4.add_element('Zn', 0.0002, percent_type='wo')
+        li4sio4.add_element('Zr', 0.0001, percent_type='wo')
 
-        # Beryllium
-        beryllium = openmc.Material(name='Beryllium')
-        beryllium.set_density('g/cm3', 1.85)
-        beryllium.add_element('Be', 98.749, percent_type='wo')
-        beryllium.add_element('O', 0.9, percent_type='wo')
-        beryllium.add_element('Al', 0.09, percent_type='wo')
-        beryllium.add_element('Fe', 0.1, percent_type='wo')
-        beryllium.add_element('Mg', 0.08, percent_type='wo')
-        beryllium.add_element('Si', 0.06, percent_type='wo')
-        beryllium.add_element('Mn', 0.01, percent_type='wo')
+        # Beryllium -- neutron multiplier
+        be = openmc.Material(name='Beryllium')
+        be.set_density('g/cm3', 1.85)
+        be.add_element('Be', 98.749, percent_type='wo')
+        be.add_element('O', 0.9, percent_type='wo')
+        be.add_element('Al', 0.09, percent_type='wo')
+        be.add_element('Fe', 0.1, percent_type='wo')
+        be.add_element('Mg', 0.08, percent_type='wo')
+        be.add_element('Si', 0.06, percent_type='wo')
+        be.add_element('Mn', 0.01, percent_type='wo')
         #Beryllium has a natural amount of Uranium that is already an activation risk
-        #when I add the number of triso's i need to accieve my desired U238 mass
-        # The whole sustem has a U238 mass > than expected due to the Uranium in Beryllium
-        beryllium.add_element('U', 0.01, percent_type='wo')
-        beryllium.add_element('Co', 0.001, percent_type='wo')
-        beryllium.add_element('Cu', 0.001, percent_type='wo')
-        beryllium.add_element('Fe', 0.003, percent_type='wo')
-        beryllium.add_element('K', 0.001, percent_type='wo')
-        beryllium.add_element('Mg', 0.0005, percent_type='wo')
-        beryllium.add_element('Mn', 0.0005, percent_type='wo')
-        beryllium.add_element('Na', 0.001, percent_type='wo')
-        beryllium.add_element('Nb', 0.001, percent_type='wo')
-        beryllium.add_element('Ni', 0.0005, percent_type='wo')
-        beryllium.add_element('Pb', 0.0005, percent_type='wo')
-        beryllium.add_element('Ta', 0.002, percent_type='wo')
+        #when I add the number of triso's i need to achieve my desired U238 mass
+        # The whole sustem has a U238 mass > than expected due to the Uranium in Beryllium --ezoccoli1
+        be.add_element('U', 0.01, percent_type='wo')
+        be.add_element('Co', 0.001, percent_type='wo')
+        be.add_element('Cu', 0.001, percent_type='wo')
+        be.add_element('Fe', 0.003, percent_type='wo')
+        be.add_element('K', 0.001, percent_type='wo')
+        be.add_element('Mg', 0.0005, percent_type='wo')
+        be.add_element('Mn', 0.0005, percent_type='wo')
+        be.add_element('Na', 0.001, percent_type='wo')
+        be.add_element('Nb', 0.001, percent_type='wo')
+        be.add_element('Ni', 0.0005, percent_type='wo')
+        be.add_element('Pb', 0.0005, percent_type='wo')
+        be.add_element('Ta', 0.002, percent_type='wo')
 
-        # ceramic TRISO -- neglect buoyancy effects --ezoccoli 2025-07-07
-        # TRISO model from OpenMC examples
-        fuel = openmc.Material(name='Fuel')
-        fuel.set_density('g/cm3', 10.5)
-        fuel.add_nuclide('U235', 4.6716e-02)
-        fuel.add_nuclide('U238', 2.8697e-01)
-        fuel.add_nuclide('O16', 5.0000e-01)
-        fuel.add_element('C', 1.6667e-01)
-        Mu238 = fuel.get_mass_density('U238')
-        Mfuel = fuel.density  # used to compute mass fraction of U238 in fuel
-        # calculate total fuel mass given a mass of U238 (self.u_list)
-
-        # SiC FCI or structural inserts AND coating for TRISO
-        SiC = openmc.Material(name='SiC')
-        SiC.set_density('g/cm3', 3.2)
-        SiC.add_element('C', 0.5)
-        SiC.add_element('Si', 0.5)
-
-        radius_fuel = 400e-4  # 400 μm = 0.0400 cm
-        radius_sic = 500e-4  # 500 μm = 0.0500 cm
-        vol_tot = (4 / 3) * np.pi * (radius_sic) ** 3
-        vol_fuel = (4 / 3) * np.pi * (radius_fuel) ** 3
-        vf_fuel = vol_fuel / vol_tot
-        vf_sic = 1.0 - vf_fuel
-
-        triso = openmc.Material.mix_materials([fuel, SiC], [vf_fuel, vf_sic], 'vo')
-        triso.set_density('g/cm3', DENSITY_TRISO)
-
+        # Helium coolant
         he = openmc.Material(name='Helium')
         he.set_density('g/cm3', 0.0001785)
         he.add_element('He', 1)
 
-        # Calculate volume ratios of TRISO vs Lithium ceramic+structure, ensure they add up to 1
+        # UO2 kernel
+        uo2 = openmc.Material(name='UO2')
+        uo2.set_density('g/cm3', 10.5)
+        uo2.add_elements_from_formula('UO2',enrichment=ENRICH_U) # (nominal ENRICH_U = 0.7204) --ppark 2025-07-23
+        mf_uo2 = ((100-ENRICH_U)*AMU_U238+ENRICH_U*AMU_U235)/((100-ENRICH_U)*AMU_U238+ENRICH_U*AMU_U235+2*AMU_O)
+        # 'mf_uo2' = mass frac of U in UO2 = 0.88 at nat U // ENRICH_U in % so subtract from 100 not 1! --ppark 2025-07-23
+
+        # SiC
+        sic = openmc.Material(name='SiC')
+        sic.set_density('g/cm3', 3.2)
+        sic.add_elements_from_formula('SiC')
+
+        # BISO -- specs and geometry from Glaser & Goldston (2012)
+        r_uo2 = 400e-4                                     # r = 400 μm = 0.0400 cm // "800 μm kernel"
+        r_sic = 500e-4                                     # 500 μm = 0.0500 cm // "100 μm thickness"
+        V_biso_particle = (4 / 3) * np.pi * (r_sic)**3     # volume of single BISO particle
+        V_uo2_in_biso   = (4 / 3) * np.pi * (r_uo2)**3     # volume of UO2 in single BISO particle
+        Vf_uo2_in_biso  = V_uo2_in_biso / V_biso_particle  # vol frac UO2 in single BISO
+        Vf_sic_in_biso  = 1.0 - Vf_uo2_in_biso             # vol frac SiC in single BISO
+        biso = openmc.Material.mix_materials([uo2, sic], [Vf_uo2_in_biso, Vf_sic_in_biso], 'vo')
+        biso.set_density('g/cm3', 6.94)                    # 6.93759 g/cc from 10.5 g/cc UO2, 3.2 g/cc SiC
+                                                           # -- ~7 g/cc from Glaser & Goldston --ppark 2025-07-22
+
+
+
+
+        # Calculate volume ratios BISO, Eurofer, Li4SiO4, Be, He -- ensure they add up to 1
         mix_list = []
-        for mtu in MASS_U_LIST_PEBBLE:
-            mass_fuel = mtu * 10**6 * (Mfuel / Mu238)
-            V_fuel = mass_fuel / 10.5 #mass over fuel density
-            V_triso = V_fuel / vf_fuel 
+        for mtu in self.u_list:
+            # Calculate volume of BISO to put in
+            m_uo2 = mtu*1e6 * (1/mf_uo2)
+            V_uo2 = m_uo2 / 10.5            # total vol of uo2 to be put in model
+            V_biso = V_uo2 / Vf_uo2_in_biso # total vol of biso to be put in model
+            Vf_biso = V_biso / VOL_CC
+            print(f"V_uo2 {V_uo2:.4f} // V_uo2_in_biso {V_uo2_in_biso:.4f}")
 
-            vf_triso = V_triso / (VOL_CC * 0.1304)
-            vf_lic = 1 - vf_triso
-            licTRISO = openmc.Material.mix_materials([lithium_ceramic, triso], [vf_lic, vf_triso], 'vo') # fractions in 'mix_materials' MUST add up to 1
-            licTRISO.volume = 0.1304 * VOL_CC
-            vf_licTRISO = 0.1304
-            vf_be = 0.379
-            vf_euro = 0.1176 #volume fractions from EU Activation Analysis Table 2
-            vf_he = 1 - (vf_licTRISO + vf_be + vf_euro)
-            mix = openmc.Material.mix_materials([licTRISO, he, eurofer, beryllium],[vf_licTRISO, vf_he, vf_euro, vf_be],'vo')
+            # Nominal volfracs minus BISO volfrac
+            Vf_eurofer = 0.1176 # Lu et al. (2017)
+            Vf_li4sio4 = 0.1304 - Vf_biso
+            Vf_be      = 0.3790
+            Vf_he      = 0.3730
+
+            mix = openmc.Material.mix_materials([eurofer, li4sio4, be, he, biso],[Vf_eurofer, Vf_li4sio4, Vf_be, Vf_he, Vf_biso],'vo')
             mix.name = 'Full Lithium Ceramic + TRISO Blanket Homogenized'
-            mix.temperature = TEMP_K
-            mix.volume = VOL_CC
-            triso.volume = V_triso
+            mix.temperature = self.temp
             mix_list.append(mix)
-
-            print(f"\nProperties of Pebble-TRISO with {mtu} MTU:")
-            print(mix)
 
         self.materials = openmc.Materials(mix_list)
         self.materials.cross_sections = set_xs_path()
@@ -226,29 +225,25 @@ class Pebble:
         self.tallies.extend([flux_tally, U_tally, Li_tally, Be_tally])
 
         
-        """First Wall Effects"""
-        sp = openmc.StatePoint(f'./OpenMC/FirstWall/statepoint.100.h5')  
-        out_tally = sp.get_tally(name='outgoing_spectrum')
-
-        energy_bins = out_tally.filters[1].bins
-        energy_bins = np.array(energy_bins)  # shape (N, 2)
-
-        energy_midpoints = 0.5 * (energy_bins[:, 0] + energy_bins[:, 1])
-
-        current_spectrum = out_tally.get_values(scores=['current']).flatten()
-
-        total_current = current_spectrum.sum()
-        probabilities = current_spectrum / total_current
-
         """ SETTINGS """
         self.settings = openmc.Settings()
 
-        """ Source
-        Isotropic 14.07 MeV point source at center of each cube
-        """
-        energies = energy_midpoints.tolist()
-        weights = probabilities.tolist()
+        """Source from First Wall Effects"""
+        # Determine a flux spectrum from the neutrons leaving the surface of a two layer first wall model './Python/FirstWall.py'
+        # The neutrons added to each MTU box in our model reflect the outgoing current spectrum of out vanadium shell
+        sp = openmc.StatePoint(f'./OpenMC/FirstWall_V4cm_900K_2025-07-22/statepoint.100.h5')
+        out_tally = sp.get_tally(name='outgoing_spectrum')
+
+        energy_bins = np.array(out_tally.filters[1].bins) # shape (N, 2)
+        energy_midpoints = 0.5 * (energy_bins[:, 0] + energy_bins[:, 1])
+
+        current_spectrum = out_tally.get_values(scores=['current']).flatten() # Neutron generation at each current in the spectrum is weighted by its probability of occurrence
+        total_current = current_spectrum.sum()
+        probabilities = current_spectrum / total_current
+
+        energies, weights = energy_midpoints.tolist(), probabilities.tolist()
         source = []
+
         for p in box_centers:
             src = openmc.IndependentSource()
             src.space  = openmc.stats.Point((p,0,0))
@@ -259,29 +254,17 @@ class Pebble:
 
         """ Run type """
         self.settings.run_mode = 'fixed source'
-        self.settings.particles = len(MASS_U_LIST) * int(1e6)  #  
+        self.settings.particles = len(self.u_list) * int(1e6)
         self.settings.batches = 100
 
 
 
     def run_openmc(self):
+        self.materials.cross_sections = set_xs_path()
         self.model = openmc.model.Model(self.geometry, self.materials, self.settings, self.tallies)
         self.model.export_to_model_xml(f"./OpenMC/{self.name}/")
         self.model.run(cwd=f"./OpenMC/{self.name}/")
 
-    def set_xs_path(self):
-        """ 
-        Temporary solution for finding xs files between WSL and Ubuntu on Computing Cluster without editing PATH --ppark 2025-06-28 
-        """
-        xs_path_ubuntu = '/opt/openmc_data/endfb-viii.0-hdf5/cross_sections.xml'
-        xs_path_wsl   = '/mnt/c/openmc/data/endfb-viii.0-hdf5/cross_sections.xml'
-        if os.path.isfile(xs_path_ubuntu):
-            self.materials.cross_sections = xs_path_ubuntu # use this on Zotacs --ppark
-        elif os.path.isfile(xs_path_wsl):
-            self.materials.cross_sections = xs_path_wsl
-            print(self.materials.cross_sections )
-        else:
-            sys.exit(f"Error finding cross section XML!")
 
 
 
