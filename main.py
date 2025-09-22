a7b0f1d8b812a2fd431daa14a59482fadc810599
Patrick Park <patrickpark910@gmail.com>
2025-09-22 09:26:07 -0400

    ARC-class benchmark

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 # Import helper functions
 from Python.reactor import *
+# from Python.plot import *
 from Python.parameters import *
 from Python.utilities import *
 
@@ -16,23 +17,23 @@ def main():
     os.makedirs(f"./OpenMC/", exist_ok=True)
 
 
-    for breeder in ['flibe']:
+    for breeder in ['arc','flibe']:
         for fertile_element in ['U']:
-            for fd_kgm3 in FERTILE_BULK_DENSITY_KGM3:
+            for fbd_kgm3 in FERTILE_BULK_DENSITY_KGM3: # [FERTILE_BULK_DENSITY_KGM3[0]]: # 
                 
-                current_run = Reactor(breeder=breeder,fertile_element=fertile_element,fertile_density_kgm3=fd_kgm3, calc_volumes=False, run_openmc=True)
+                current_run = Reactor(breeder=breeder,fertile_element=fertile_element,fertile_bulk_density_kgm3=fbd_kgm3, calc_volumes=False, run_openmc=True)
 
                 print(f"Check if '{current_run.path}' exists: {os.path.isdir(current_run.path)}")
 
                 if current_run.calc_volumes:
                     if os.path.exists(f"{current_run.path}/volume_1.h5"):
-                        print(f"{Colors.YELLOW}Warning. {Colors.END}File {current_run.path}/volume_1.h5 already exists, so this OpenMC volume calculation will be skipped...")
+                        print(f"{Colors.YELLOW}Warning.{Colors.END} File {current_run.path}/volume_1.h5 already exists, so this OpenMC volume calculation will be skipped...")
                     else:
                         current_run.volumes()
 
                 if current_run.run_openmc:
-                    if os.path.isdir(current_run.path):
-                        print(f"{Colors.YELLOW}Warning. {Colors.END}Directory {current_run.path} already exists, so this OpenMC run will be skipped...")
+                    if has_statepoint_file(current_run.path):
+                        print(f"{Colors.YELLOW}Warning.{Colors.END} File {current_run.path}/statepoint.h5 already exists, so this OpenMC run will be skipped...")
                         continue
                     else:
                         current_run.openmc()
