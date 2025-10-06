5bba1aca412130b4f09952e335f2c0ce0b93bb10
Patrick Park <patrickpark910@gmail.com>
2025-10-06 03:13:39 -0400

    refactored using Abstract Base Classes

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ import pandas as pd
 
 # Import helper functions
 from Python.reactor import *
-# from Python.plot import *
+from Python.arc     import *
+from Python.flibe   import *
+from Python.pbli    import *
 from Python.parameters import *
 from Python.utilities import *
 
@@ -26,8 +28,9 @@ def main():
 
 
     if run_type == 'plot':
-        for breeder in ['arc','flibe','ll']:
-            current_run = Reactor(breeder=breeder, run_type='plot')
+        for breeder in ['ARC','FLiBe','LL']: # make this match class name
+            
+            current_run = build_reactor(breeder, breeder_name=breeder, run_type='plot')
 
             if os.path.exists(f"{current_run.path}/{current_run.breeder_name}_xz.ppm"):
                 print(f"{Colors.YELLOW}Warning.{Colors.END} File {current_run.path}/{current_run.breeder_name}_xz.ppm already exists, so this OpenMC volume calculation will be skipped...")
@@ -36,28 +39,26 @@ def main():
 
 
     elif run_type == 'volume':
-        for breeder in ['arc','flibe','ll']:
-            current_run = Reactor(breeder=breeder, run_type='volume')
+        for breeder in ['ARC','FLiBe','LL']: # make this match class name
 
+            current_run = build_reactor(breeder, breeder_name=breeder, run_type='volume')
+            
             if os.path.exists(f"{current_run.path}/volume_1.h5"):
                 print(f"{Colors.YELLOW}Warning.{Colors.END} File {current_run.path}/volume_1.h5 already exists, so this OpenMC volume calculation will be skipped...")
             else:
                 current_run.volumes()
 
+
     elif run_type == 'tallies':
 
-        for breeder in ['arc','flibe','ll']: # 'arc','flibe',
+        for breeder in ['ARC','FLiBe','LL']: # make this match class name
             for fertile_element in ['U']:
                 for fbd_kgm3 in FERTILE_BULK_DENSITY_KGM3: # [FERTILE_BULK_DENSITY_KGM3[0]]: # 
-
-                    cv, pp = False, False
-                    #if fbd_kgm3 == 0:
-                    #    cv, pp = True, True
                     
-                    current_run = Reactor(breeder=breeder,
-                                          fertile_element=fertile_element,
-                                          fertile_bulk_density_kgm3=fbd_kgm3, 
-                                          run_type='tallies')
+                    current_run = build_reactor(breeder, breeder_name=breeder,
+                                                fertile_element=fertile_element,
+                                                fertile_bulk_density_kgm3=fbd_kgm3, 
+                                                run_type='tallies')
 
                     print(f"Check if '{current_run.path}' exists: {os.path.isdir(current_run.path)}")
 
@@ -66,9 +67,23 @@ def main():
                     else:
                         current_run.openmc()
 
-                
+                    # current_run.extract_tallies()
+
+
+def build_reactor(breeder:str, **kwargs):
+    """
+    Create reactor model from mapping into abstract base class (ABC)
+    """
+    try:
+        cls = eval(breeder) 
+        reactor = cls(**kwargs)
+        reactor.initialize()
+    except KeyError:
+        raise ValueError(f"Unknown breeder '{breeder}'. Expected one of {list(BREEDER_CLASSES)}")
+    return reactor
 
 
 
 if __name__ == '__main__':
+
     main()
