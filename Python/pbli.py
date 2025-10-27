013667f6d0e11880a76a4e347d76444a39504f49
patrickpark910 <patrickpark910@gmail.com>
2025-10-27 19:18:12 -0400

    changing LL blanket geometry

--- Python/pbli.py
+++ Python/pbli.py
@@ -70,115 +70,78 @@ class LL(Reactor):
         # ------------------------------------------------------------------
 
         # First wall front (F82H steel)
-        self.structure = openmc.Material(name='firstwall_front', temperature=self.temp_k)
-        self.structure.depletable = False
-        self.structure.add_nuclide("C12",    0.00040000000, "ao")
-        self.structure.add_nuclide("Si28",  0.00015679100, "ao")
-        self.structure.add_nuclide("Si29",  0.00000795600, "ao")
-        self.structure.add_nuclide("Si30",  0.00000525300, "ao")
-        self.structure.add_nuclide("V51",   0.00018400000, "ao")
-        self.structure.add_nuclide("Cr50",  0.00031327500, "ao")
-        self.structure.add_nuclide("Cr52",  0.00604119000, "ao")
-        self.structure.add_nuclide("Cr53",  0.00068502200, "ao")
-        self.structure.add_nuclide("Cr54",  0.00017051700, "ao")
-        self.structure.add_nuclide("Mn55",  0.00008600000, "ao")
-        self.structure.add_nuclide("Fe54",  0.00438860000, "ao")
-        self.structure.add_nuclide("Fe56",  0.06889170000, "ao")
-        self.structure.add_nuclide("Fe57",  0.00159101000, "ao")
-        self.structure.add_nuclide("Fe58",  0.00021173400, "ao")
-        self.structure.add_nuclide("Ta181",  0.00001000000, "ao") 
-        self.structure.add_nuclide("W182",  0.00013515000, "ao")
-        self.structure.add_nuclide("W183",  0.00007298100, "ao")
-        self.structure.add_nuclide("W184",  0.00015626400, "ao")
-        self.structure.add_nuclide("W186",  0.00014499300, "ao")
-        self.structure.set_density("atom/b-cm", 0.08365243600)  # from MCNP cell 112
+        self.steel = openmc.Material(name='f82h', temperature=self.temp_k)
+        self.steel.depletable = False
+        self.steel.add_nuclide("C12",    0.00040000000, "ao")
+        self.steel.add_nuclide("Si28",  0.00015679100, "ao")
+        self.steel.add_nuclide("Si29",  0.00000795600, "ao")
+        self.steel.add_nuclide("Si30",  0.00000525300, "ao")
+        self.steel.add_nuclide("V51",   0.00018400000, "ao")
+        self.steel.add_nuclide("Cr50",  0.00031327500, "ao")
+        self.steel.add_nuclide("Cr52",  0.00604119000, "ao")
+        self.steel.add_nuclide("Cr53",  0.00068502200, "ao")
+        self.steel.add_nuclide("Cr54",  0.00017051700, "ao")
+        self.steel.add_nuclide("Mn55",  0.00008600000, "ao")
+        self.steel.add_nuclide("Fe54",  0.00438860000, "ao")
+        self.steel.add_nuclide("Fe56",  0.06889170000, "ao")
+        self.steel.add_nuclide("Fe57",  0.00159101000, "ao")
+        self.steel.add_nuclide("Fe58",  0.00021173400, "ao")
+        self.steel.add_nuclide("Ta181",  0.00001000000, "ao") 
+        self.steel.add_nuclide("W182",  0.00013515000, "ao")
+        self.steel.add_nuclide("W183",  0.00007298100, "ao")
+        self.steel.add_nuclide("W184",  0.00015626400, "ao")
+        self.steel.add_nuclide("W186",  0.00014499300, "ao")
+        self.steel.set_density("atom/b-cm", 0.08365243600)  # from MCNP cell 112
 
-        self.coolant = openmc.Material(name="coolant", temperature=self.temp_k)
-        self.coolant.depletable = False
-        self.coolant.add_nuclide("He4", 0.00049800000, "ao")
-        self.coolant.add_nuclide("C12",  0.00006800000, "ao")
-        self.coolant.add_nuclide("Si28",0.00002665450, "ao")
-        self.coolant.add_nuclide("Si29",0.00000135250, "ao")
-        self.coolant.add_nuclide("Si30",0.00000089300, "ao")
-        self.coolant.add_nuclide("V51", 0.00003128000, "ao")
-        self.coolant.add_nuclide("Cr50",0.00005325680, "ao")
-        self.coolant.add_nuclide("Cr52",0.00102700000, "ao")
-        self.coolant.add_nuclide("Cr53",0.00011645400, "ao")
-        self.coolant.add_nuclide("Cr54",0.00002898790, "ao")
-        self.coolant.add_nuclide("Mn55",0.00001462000, "ao")
-        self.coolant.add_nuclide("Fe54",0.00074606200, "ao")
-        self.coolant.add_nuclide("Fe56",0.01171160000, "ao")
-        self.coolant.add_nuclide("Fe57",0.00027047200, "ao")
-        self.coolant.add_nuclide("Fe58",0.00003599480, "ao")
-        self.coolant.add_nuclide("Ta181",0.00000170000, "ao")
-        self.coolant.add_nuclide("W182",0.00002297550, "ao")
-        self.coolant.add_nuclide("W183",0.00001240680, "ao")
-        self.coolant.add_nuclide("W184",0.00002656490, "ao")
-        self.coolant.add_nuclide("W186",0.00002464880, "ao")
-        self.coolant.set_density("atom/b-cm", 0.01471892350)
+        # Helium-4 gas 
+        helium = openmc.Material(name='helium', temperature=self.temp_k)
+        helium.depletable = False
+        helium.add_nuclide("He4", 1, "ao")
+
+        # SiC coating
+        sic = openmc.Material(name='SiC')
+        sic.depletable = False
+        sic.add_elements_from_formula('SiC')
+        sic.set_density('g/cm3', 3.2)
+
+
+        # ------------------------------------------------------------------
+        # Coolant (17 vol% ferritic steel + 83 vol% Helium-4)
+        # ------------------------------------------------------------------
+
+        self.coolant = openmc.Material.mix_materials([self.steel, helium], [0.17, 0.83], 'vo')
+        self.coolant.set_density("atom/b-cm", 0.01471892350) # from MCNP
+        self.coolant.name, self.coolant.temperature = "coolant (17 vol% fs  83 vol% he-4)", self.temp_k
 
 
         # ------------------------------------------------------------------
         # Breeder material
         # ------------------------------------------------------------------
 
-        self.breeder = openmc.Material(name='breeder', temperature=self.temp_k)
-        self.breeder.set_density('g/cm3', self.breeder_density)
-        self.breeder.add_element('Pb', 0.83, percent_type='ao') 
-        self.breeder.add_element('Li', 0.17, percent_type='ao', enrichment_target='Li6', enrichment_type='wo', enrichment=self.breeder_enrich) # Li-6 enrichment to 90% 
+        pbli = openmc.Material(name='breeder', temperature=self.temp_k)
+        pbli.set_density('g/cm3', self.breeder_density)
+        pbli.add_element('Pb', 0.83, percent_type='ao') 
+        pbli.add_element('Li', 0.17, percent_type='ao', enrichment_target='Li6', enrichment_type='wo', enrichment=self.breeder_enrich) # Li-6 enrichment to 90% 
+
+        self.breeder = openmc.Material.mix_materials([self.steel, pbli, sic, helium], [0.019, 0.808, 0.076, 0.097], 'vo')
+        # self.breeder.set_density("atom/b-cm", 0.01471892350) # from MCNP
+        self.breeder.name, self.breeder.temperature = "Breeder (1.9 vol% FS + 80.8 vol% LL + 7.6 vol% SiC + 9.7 vol% He-4)", self.temp_k    
+
 
         # ------------------------------------------------------------------
         # Divider material
         # ------------------------------------------------------------------
         
-        self.divider = openmc.Material(name="divider1", temperature=self.temp_k)
+        self.divider = openmc.Material.mix_materials([self.steel, helium], [0.512, 0.488], 'vo')
         self.divider.depletable = False
-        self.divider.add_nuclide("He4", 0.00029280000, "ao")
-        self.divider.add_nuclide("C12", 0.00020480000, "ao")
-        self.divider.add_nuclide("Si28", 0.00008027700, "ao")
-        self.divider.add_nuclide("Si29", 0.00000407347, "ao")
-        self.divider.add_nuclide("Si30", 0.00000268954, "ao")
-        self.divider.add_nuclide("V51", 0.00009420800, "ao")
-        self.divider.add_nuclide("Cr50", 0.00016039700, "ao")
-        self.divider.add_nuclide("Cr52", 0.00309309000, "ao")
-        self.divider.add_nuclide("Cr53", 0.00035073100, "ao")
-        self.divider.add_nuclide("Cr54", 0.00008730470, "ao")
-        self.divider.add_nuclide("Mn55", 0.00004403200, "ao")
-        self.divider.add_nuclide("Fe54", 0.00224696000, "ao")
-        self.divider.add_nuclide("Fe56", 0.03527260000, "ao")
-        self.divider.add_nuclide("Fe57", 0.00081459700, "ao")
-        self.divider.add_nuclide("Fe58", 0.00010840800, "ao")
-        self.divider.add_nuclide("Ta181", 0.00000512000, "ao")
-        self.divider.add_nuclide("W182", 0.00006919680, "ao")
-        self.divider.add_nuclide("W183", 0.00003736630, "ao")
-        self.divider.add_nuclide("W184", 0.00008000720, "ao")
-        self.divider.add_nuclide("W186", 0.00007423640, "ao")
-        self.divider.set_density("atom/b-cm", 0.04312289441)
+        self.divider.name, self.divider.temperature = "divider (51.2 vol% FS, 48.8 vol% He-4)", self.temp_k
+        # self.divider.set_density("atom/b-cm", 0.04312289441)
 
         #--INNER MANIFOLD-- 
-        self.inner_manifold = openmc.Material(name="inner_manifold", temperature=self.temp_k)
-        self.inner_manifold.depletable = False
-        self.inner_manifold.add_nuclide("He4", 0.00032820000, "ao")
-        self.inner_manifold.add_nuclide("C12", 0.00018120000, "ao")
-        self.inner_manifold.add_nuclide("Si28", 0.00007102630, "ao")
-        self.inner_manifold.add_nuclide("Si29", 0.00000360407, "ao")
-        self.inner_manifold.add_nuclide("Si30", 0.00000237961, "ao")
-        self.inner_manifold.add_nuclide("V51", 0.00008335200, "ao")
-        self.inner_manifold.add_nuclide("Cr50", 0.00014191400, "ao")
-        self.inner_manifold.add_nuclide("Cr52", 0.00273666000, "ao")
-        self.inner_manifold.add_nuclide("Cr53", 0.00031031500, "ao")
-        self.inner_manifold.add_nuclide("Cr54", 0.00007724420, "ao")
-        self.inner_manifold.add_nuclide("Mn55", 0.00003895800, "ao")
-        self.inner_manifold.add_nuclide("Fe54", 0.00198804000, "ao")
-        self.inner_manifold.add_nuclide("Fe56", 0.03120790000, "ao")
-        self.inner_manifold.add_nuclide("Fe57", 0.00072072800, "ao")
-        self.inner_manifold.add_nuclide("Fe58", 0.00009591550, "ao")
-        self.inner_manifold.add_nuclide("Ta181", 0.00000453000, "ao")
-        self.inner_manifold.add_nuclide("W182", 0.00006122300, "ao")
-        self.inner_manifold.add_nuclide("W183", 0.00003306040, "ao")
-        self.inner_manifold.add_nuclide("W184", 0.00007078760, "ao")
-        self.inner_manifold.add_nuclide("W186", 0.00006568180, "ao")
-        self.inner_manifold.set_density("atom/b-cm", 0.03822271948)
+        self.manifold = openmc.Material.mix_materials([self.steel, helium], [0.453, 0.547], 'vo')
+        self.manifold.depletable = False
+        self.manifold.name, self.manifold.temperature = "inner manifold (45.3 vol% FS, 54.7 vol% He-4)", self.temp_k
+        # self.manifold.set_density("atom/b-cm", 0.03822271948)
 
 
         # ------------------------------------------------------------------
@@ -191,63 +154,32 @@ class LL(Reactor):
             # UO2 kernel (enriched uranium)
             uo2 = openmc.Material(name='UO2')
             uo2.add_elements_from_formula('UO2', enrichment=ENRICH_U)
-            uo2.set_density('g/cm3', 10.5)  # nominal UO2 density
-
-            # SiC coating
-            sic = openmc.Material(name='SiC')
-            sic.add_elements_from_formula('SiC')
-            sic.set_density('g/cm3', 3.2)
-
-            # Glaser & Goldston BISO particles used for simplicity
-            # and to validate our results with their model
-            # BISO particles include our fuel pellet coated in one layer of SiC
-
-            r_uo2 = 400e-4  # r = 400 μm = 0.0400 cm // "800 μm kernel"
-            r_sic = 500e-4  # 500 μm = 0.0500 cm // "100 μm thickness"
-            V_biso_particle = (4 / 3) * np.pi * (r_sic)**3     # volume of single BISO particle
-            V_uo2_in_biso   = (4 / 3) * np.pi * (r_uo2)**3     # volume of UO2 in single BISO particle
-            Vf_uo2_in_biso  = V_uo2_in_biso / V_biso_particle  # vol frac UO2 in single BISO
-            Vf_sic_in_biso  = 1.0 - Vf_uo2_in_biso             # vol frac SiC in single BISO
-
-            biso = openmc.Material.mix_materials([uo2, sic], [Vf_uo2_in_biso, Vf_sic_in_biso], 'vo')
-            # biso.set_density('g/cm3', DENSITY_BISO)  # turned off --ppark 2025-10-24
-
-            self.fertile = biso
+            uo2.set_density('g/cm3', DENSITY_UO2)  # nominal UO2 density
 
         if self.fertile_element == 'Th':
+
             # ThO2 kernel
             kernel = openmc.Material(name='ThO2')
             kernel.add_elements_from_formula('ThO2')
-            kernel.set_density('g/cm3', 10.0)  # nominal UO2 density
+            kernel.set_density('g/cm3', DENSITY_ThO2)  # nominal UO2 density
 
-            # SiC coating
-            sic = openmc.Material(name='SiC')
-            sic.add_elements_from_formula('SiC')
-            sic.set_density('g/cm3', 3.2)
-
-            # BISO = UO2/ThO2 kernel + 1 SiC coat
-            r_kernel = 400e-4  # r = 400 μm = 0.0400 cm // "800 μm kernel"
-            r_sic = 500e-4     # r = 500 μm = 0.0500 cm // "100 μm thickness"
-            V_biso_particle = (4 / 3) * np.pi * (r_sic)**3           # volume of single BISO particle
-            V_kernel_in_biso   = (4 / 3) * np.pi * (r_kernel)**3     # volume of UO2/ThO2 kernel in single BISO particle
-            Vf_kernel_in_biso  = V_kernel_in_biso / V_biso_particle  # vol frac kernel in single BISO
-            Vf_sic_in_biso     = 1.0 - Vf_kernel_in_biso             # vol frac SiC in single BISO
-
-            biso = openmc.Material.mix_materials([kernel, sic], [Vf_kernel_in_biso, Vf_sic_in_biso], 'vo')
-            # biso.set_density('g/cm3', DENSITY_BISO_ThO2)  
-
-            self.fertile = biso
+        biso = openmc.Material.mix_materials([kernel, sic], [BISO_KERNEL_VOL_FRAC, BISO_COAT_VOL_FRAC], 'vo')
+        self.fertile = biso
 
 
         # ------------------------------------------------------------------
         # Breeder and fertile material mixed in the blanket breeding regions 
         # ------------------------------------------------------------------
 
-        breeder_vol_frac, biso_vol_frac = calc_biso_blanket_vol_fracs(self.fertile_bulk_density_kgm3,
-                                                                      self.breeder_volume,
-                                                                      fertile_element=self.fertile_element,
-                                                                      fertile_enrich=ENRICH_U if self.fertile_element == 'U' else 0.0)
-        self.blanket = openmc.Material.mix_materials([self.breeder, self.fertile], [breeder_vol_frac, biso_vol_frac], 'vo')
+        pbli_vol_frac, biso_vol_frac = calc_biso_blanket_vol_fracs(self.fertile_bulk_density_kgm3,
+                                                                   0.808*self.breeder_volume, # volume dedicated to lead-lithium is only 80.8 vol% of the total breeder region volume (see Glaser & Goldston 2012)
+                                                                   fertile_element=self.fertile_element,
+                                                                   fertile_enrich=ENRICH_U if self.fertile_element == 'U' else 0.0)
+        
+        
+        self.breeder = openmc.Material.mix_materials([self.breeder, self.fertile], [pbli_vol_frac, biso_vol_frac], 'vo')
+        
+        
         self.blanket.name, self.blanket.temperature = self.name, self.temp_k
 
 
@@ -255,7 +187,7 @@ class LL(Reactor):
         # Add materials 
         # ------------------------------------------------------------------
 
-        self.materials = openmc.Materials([self.firstwall, self.structure, self.coolant, self.divider, self.inner_manifold, self.blanket]) # check if need to add self.backplate
+        self.materials = openmc.Materials([self.firstwall, self.structure, self.coolant, self.divider, self.manifold, self.blanket]) # check if need to add self.backplate
 
 
     def geometry(self):
@@ -333,10 +265,10 @@ class LL(Reactor):
         cell_br1  = openmc.Cell(cell_id=31, region= +self.surface_fwb & -self.surface_br1, fill=self.blanket)
         cell_d1   = openmc.Cell(cell_id=23, region= +self.surface_br1 & -self.surface_d1,  fill=self.divider)
         cell_br2  = openmc.Cell(cell_id=32, region= +self.surface_d1  & -self.surface_br2, fill=self.blanket)
-        cell_im_i   = openmc.Cell(cell_id=24, region= -dividing_cylinder & +self.surface_br2 & -self.surface_im_i, fill=self.inner_manifold)    # inboard manifold
+        cell_im_i   = openmc.Cell(cell_id=24, region= -dividing_cylinder & +self.surface_br2 & -self.surface_im_i, fill=self.manifold)    # inboard manifold
         cell_d2_o   = openmc.Cell(cell_id=25, region= +dividing_cylinder & +self.surface_br2 & -self.surface_d2_o,   fill=self.divider)         # outboard divider
         cell_br3_o  = openmc.Cell(cell_id=33, region= +dividing_cylinder & +self.surface_d2_o & -self.surface_br3_o, fill=self.blanket)         # outboard blanket module
-        cell_im_o   = openmc.Cell(cell_id=26, region= +dividing_cylinder & +self.surface_br3_o & -self.surface_im_o, fill=self.inner_manifold)  # outboard manifold
+        cell_im_o   = openmc.Cell(cell_id=26, region= +dividing_cylinder & +self.surface_br3_o & -self.surface_im_o, fill=self.manifold)  # outboard manifold
 
         # Void cells
         cell_void_i = openmc.Cell(cell_id=99, region= +self.surface_im_i & -dividing_cylinder & -outer_cylinder & +bottom_plane & -top_plane)
