5ee278571e06f5c186c0d9ce2187eee839f0e822
patrickpark910 <patrickpark910@gmail.com>
2025-10-28 10:37:06 -0400

    testing biso self-shielding

--- plot.py
+++ plot.py
@@ -16,12 +16,12 @@ from Python.parameters import *
 def plot_all():
     """ Read CSV data into pandas DataFrames """
 
-    combined_plot = Plot(save=True, show=True)
+    combined_plot = Plot(save=False, show=True)
     
     """ Pick which one to plot by uncommenting """
     combined_plot.plot_tbr()
-    combined_plot.plot_pu_per_yr()
-    combined_plot.plot_cum_norm_histogram()
+    # combined_plot.plot_pu_per_yr()
+    # combined_plot.plot_cum_norm_histogram()
 
     print("All plots completed and saved.")
 
@@ -58,12 +58,12 @@ class Plot:
         print(f"\nPlotting tritium breeding ratio vs. fertile density...")
 
         # Setting up x, y separately here so you can remove the impurity/wppm-magnitude cases --ppark 2025-08-06
-        x6, y6 =     self.pb_u_rr_df['fertile_kg/m3'],     self.pb_u_rr_df['Li6(n,t)'] +     self.pb_u_rr_df['Li7(n,Xt)']
-        x5, y5 =    self.pb_th_rr_df['fertile_kg/m3'],    self.pb_th_rr_df['Li6(n,t)'] +    self.pb_th_rr_df['Li7(n,Xt)']     
-        x4, y4 =   self.pbli_u_rr_df['fertile_kg/m3'],   self.pbli_u_rr_df['Li6(n,t)'] +   self.pbli_u_rr_df['Li7(n,Xt)']
-        x3, y3 =  self.pbli_th_rr_df['fertile_kg/m3'],  self.pbli_th_rr_df['Li6(n,t)'] +  self.pbli_th_rr_df['Li7(n,Xt)']
-        x2, y2 =  self.flibe_u_rr_df['fertile_kg/m3'],  self.flibe_u_rr_df['Li6(n,t)'] +  self.flibe_u_rr_df['Li7(n,Xt)']
-        x1, y1 = self.flibe_th_rr_df['fertile_kg/m3'], self.flibe_th_rr_df['Li6(n,t)'] + self.flibe_th_rr_df['Li7(n,Xt)']
+        x6, y6 =     self.pb_u_rr_df['fertile_kg/m3'], BLANKET_COVERAGE * (    self.pb_u_rr_df['Li6(n,t)'] +     self.pb_u_rr_df['Li7(n,Xt)'] )
+        x5, y5 =    self.pb_th_rr_df['fertile_kg/m3'], BLANKET_COVERAGE * (   self.pb_th_rr_df['Li6(n,t)'] +    self.pb_th_rr_df['Li7(n,Xt)'] )
+        x4, y4 =   self.pbli_u_rr_df['fertile_kg/m3'], BLANKET_COVERAGE * (  self.pbli_u_rr_df['Li6(n,t)'] +   self.pbli_u_rr_df['Li7(n,Xt)'] )
+        x3, y3 =  self.pbli_th_rr_df['fertile_kg/m3'], BLANKET_COVERAGE * ( self.pbli_th_rr_df['Li6(n,t)'] +  self.pbli_th_rr_df['Li7(n,Xt)'] )
+        x2, y2 =  self.flibe_u_rr_df['fertile_kg/m3'], BLANKET_COVERAGE * ( self.flibe_u_rr_df['Li6(n,t)'] +  self.flibe_u_rr_df['Li7(n,Xt)'] )
+        x1, y1 = self.flibe_th_rr_df['fertile_kg/m3'], BLANKET_COVERAGE * (self.flibe_th_rr_df['Li6(n,t)'] + self.flibe_th_rr_df['Li7(n,Xt)'] )
 
         # AkimaInterpolation ripped from my ELWR paper --ppark 2025-08-06
         x_fine = np.linspace(x1.min(), x1.max(), 300) # Evaluate on a fine grid
@@ -81,6 +81,12 @@ class Plot:
 
 
         plt.figure(figsize=(7.5,5))
+
+        # Tick grid
+        ax = plt.gca()
+
+        # ax.axhspan(.9,1, color='#e0ded8') # color='#e0ded8', # alpha=0.3)
+        ax.axhspan(0,1.00, color='#e0ded8') # color='#f7f7f6' # , alpha=0.3)
 
         plt.scatter(x6, y6, marker='s', s=40, color='#b41f24') #  PB-UO2
         plt.scatter(x5, y5, marker='1', s=70, color='#b41f24') #  PB-ThO2
@@ -105,22 +111,20 @@ class Plot:
         plt.plot([9e8,9e9], [9e8,9e9], '+--', markersize=8, linewidth=1, color='#66b420',   label=r'FLiBe-ThF$_4$') #  
         
         
-        # plt.title(f'Tritium breeding ratio (All)') # Exclude title for production figs --ppark 2025-08-06
+        plt.title(r'TBRs going up to 1000 kg$/$m$^3$ (2025-10-28)') # Exclude title for production figs --ppark 2025-08-06
         plt.xlabel(r'Fertile isotope density in blanket [kg$/$m$^3$]')
         plt.ylabel('Tritium breeding ratio')
 
-        plt.xlim(-5,165)
-        plt.ylim(1.03,1.47) # plt.ylim(0.98,1.42)
-        
-        # Tick grid
-        ax = plt.gca()
+        plt.xlim(-25,1025)  # plt.xlim(-5,165)
+        plt.ylim(.89,1.16)  # plt.ylim(0.98,1.42)
         ax.xaxis.set_ticks_position('both')
+        ax.xaxis.set_major_locator(MultipleLocator(100))
+        ax.xaxis.set_minor_locator(MultipleLocator(50))
         ax.yaxis.set_ticks_position('both')
+        ax.yaxis.set_major_locator(MultipleLocator(0.05))
         ax.yaxis.set_minor_locator(MultipleLocator(0.01))
         ax.grid(axis='x', which='major', linestyle='-', linewidth=0.5)
         ax.grid(axis='y', which='major', linestyle='-', linewidth=0.5)
-        # ax.axhspan(.9,1, color='#e0ded8') # color='#e0ded8', # alpha=0.3)
-        ax.axhspan(1,1.05, color='#e0ded8') # color='#f7f7f6' # , alpha=0.3)
 
         plt.tight_layout()
 
@@ -128,8 +132,8 @@ class Plot:
         leg.get_frame().set_linewidth(0.5) 
 
         if self.save:
-            plt.savefig(f'./Figures/pdf/fig_tbr.pdf', bbox_inches='tight', format='pdf')
-            plt.savefig(f'./Figures/png/fig_tbr.png', bbox_inches='tight', format='png')
+            plt.savefig(f'./Figures/pdf/fig_tbr_1000kgm3_ALX.pdf', bbox_inches='tight', format='pdf')
+            plt.savefig(f'./Figures/png/fig_tbr_1000kgm3_ALX.png', bbox_inches='tight', format='png')
             print(f"Exported tritium breeding ratio plots.")
         else:
             print(f"Did not export tritium breeding ratio plots.")
@@ -152,12 +156,12 @@ class Plot:
         u233_at_to_kg_per_yr  = tot_n_per_yr / AVO * AMU_U233  / 1e3
         
         # Setting up x, y separately here so you can remove the impurity/wppm-magnitude cases --ppark 2025-08-06
-        x6, y6 =     self.pb_u_rr_df['fertile_kg/m3'], pu239_at_to_kg_per_yr *     self.pb_u_rr_df['U238(n,g)'] 
-        x5, y5 =    self.pb_th_rr_df['fertile_kg/m3'],  u233_at_to_kg_per_yr *    self.pb_th_rr_df['Th232(n,g)']
-        x4, y4 =   self.pbli_u_rr_df['fertile_kg/m3'], pu239_at_to_kg_per_yr *   self.pbli_u_rr_df['U238(n,g)'] 
-        x3, y3 =  self.pbli_th_rr_df['fertile_kg/m3'],  u233_at_to_kg_per_yr *  self.pbli_th_rr_df['Th232(n,g)']
-        x2, y2 =  self.flibe_u_rr_df['fertile_kg/m3'], pu239_at_to_kg_per_yr *  self.flibe_u_rr_df['U238(n,g)']
-        x1, y1 = self.flibe_th_rr_df['fertile_kg/m3'],  u233_at_to_kg_per_yr * self.flibe_th_rr_df['Th232(n,g)']
+        x6, y6 =     self.pb_u_rr_df['fertile_kg/m3'], BLANKET_COVERAGE * pu239_at_to_kg_per_yr *     self.pb_u_rr_df['U238(n,g)'] 
+        x5, y5 =    self.pb_th_rr_df['fertile_kg/m3'], BLANKET_COVERAGE *  u233_at_to_kg_per_yr *    self.pb_th_rr_df['Th232(n,g)']
+        x4, y4 =   self.pbli_u_rr_df['fertile_kg/m3'], BLANKET_COVERAGE * pu239_at_to_kg_per_yr *   self.pbli_u_rr_df['U238(n,g)'] 
+        x3, y3 =  self.pbli_th_rr_df['fertile_kg/m3'], BLANKET_COVERAGE *  u233_at_to_kg_per_yr *  self.pbli_th_rr_df['Th232(n,g)']
+        x2, y2 =  self.flibe_u_rr_df['fertile_kg/m3'], BLANKET_COVERAGE * pu239_at_to_kg_per_yr *  self.flibe_u_rr_df['U238(n,g)']
+        x1, y1 = self.flibe_th_rr_df['fertile_kg/m3'], BLANKET_COVERAGE *  u233_at_to_kg_per_yr * self.flibe_th_rr_df['Th232(n,g)']
 
         # AkimaInterpolation ripped from my ELWR paper --ppark 2025-08-06
         x_fine = np.linspace(x1.min(), x1.max(), 300) # Evaluate on a fine grid
@@ -193,22 +197,22 @@ class Plot:
         plt.plot([9e8,9e9], [9e8,9e9], '^-',  markersize=6, linewidth=1, color='#0047ba', label=r'LL-UO$_2$')     #  blue: #0047ba
 
 
-        # plt.title(f'Tritium breeding ratio (All)') # Exclude title for production figs --ppark 2025-08-06
+        plt.title(r'Fissile prod rates up to 1000 kg$/$m$^3$ (2025-10-28)')  # Exclude title for production figs --ppark 2025-08-06
         plt.xlabel(r'Fertile isotope density in blanket [kg$/$m$^3$]')
         plt.ylabel('Initial fissile production rate [kg$/$yr]')
 
-        plt.xlim(-5,165)
-        plt.ylim(-15,2*(250+7.5))  # plt.ylim(-7.5,225+7.5) 
+        plt.xlim(-25,1025) # plt.xlim(-5,165)
+        plt.ylim(-25,1425) # plt.ylim(-15,2*(250+7.5))  # plt.ylim(-7.5,225+7.5) 
 
         # Tick grid
         ax = plt.gca()
         ax.xaxis.set_ticks_position('both')
-        ax.xaxis.set_minor_locator(MultipleLocator(10))
+        ax.xaxis.set_minor_locator(MultipleLocator(100)) # ax.xaxis.set_minor_locator(MultipleLocator(10))
         ax.grid(axis='x', which='major', linestyle='-', linewidth=0.5)
  
         ax.yaxis.set_ticks_position('both')
-        ax.yaxis.set_major_locator(MultipleLocator(50))   # ax.yaxis.set_major_locator(MultipleLocator(25))
-        ax.yaxis.set_minor_locator(MultipleLocator(25)) # ax.yaxis.set_minor_locator(MultipleLocator(12.5))
+        ax.yaxis.set_major_locator(MultipleLocator(200)) # ax.yaxis.set_major_locator(MultipleLocator(50))   # ax.yaxis.set_major_locator(MultipleLocator(25))
+        ax.yaxis.set_minor_locator(MultipleLocator(50)) # ax.yaxis.set_minor_locator(MultipleLocator(25))   # ax.yaxis.set_minor_locator(MultipleLocator(12.5))
         ax.grid(axis='y', which='major', linestyle='-', linewidth=0.5)
 
         plt.tight_layout()
@@ -217,8 +221,8 @@ class Plot:
         leg.get_frame().set_linewidth(0.5) 
     
         if self.save:
-            plt.savefig(f'./Figures/pdf/fig_fissile_per_yr.pdf', bbox_inches='tight', format='pdf')
-            plt.savefig(f'./Figures/png/fig_fissile_per_yr.png', bbox_inches='tight', format='png')
+            plt.savefig(f'./Figures/pdf/fig_fissile_per_yr_1000kgm3.pdf', bbox_inches='tight', format='pdf')
+            plt.savefig(f'./Figures/png/fig_fissile_per_yr_1000kgm3.png', bbox_inches='tight', format='png')
             print("Exported fissile production per year plot for all blankets.")
         else:
             print("Did not export fissile production per year plot due to user setting.")
