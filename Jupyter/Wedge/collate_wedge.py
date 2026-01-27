3d0368407d302c546141bfb8b74b4bebab6ea832
Greta Li <greta.li@princeton.edu>
2026-01-27 13:38:53 -0500

    cumulative histogram for prism C

--- Jupyter/Wedge/collate_wedge.py
+++ Jupyter/Wedge/collate_wedge.py
@@ -2,6 +2,8 @@ import os, sys
 import openmc
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, ScalarFormatter, FuncFormatter
+import numpy as np
 
 current_dir = os.getcwd()
 print(current_dir)
@@ -41,6 +43,7 @@ def extract_tallies(case='C', fertile_isotope='U238', fertile_loading=30):
 # COLLATE TALLIES ###########################################
 def collate_ng_tallies():
     tally_folders = [x for x in os.listdir("./Jupyter/Wedge/OpenMC/") if (x.startswith(f"prism_C_U238_"))]
+    print(tally_folders)
     ng_list = []
     fertile_isotope='U238'
     for folder in tally_folders:
@@ -69,101 +72,103 @@ def collate_ng_tallies():
 
     df_ng_collated.to_csv(f"./Figures/Data/prism_C_{fertile_isotope}_n-gamma_{900}K.csv",index=False)
 
-# PLOT WEDGE ################################################
-def plot_cum_norm_histogram(self):
-    """
-    Plots cumulative, normalized Pu production vs. energy for contours of MTU.
-    """
+# PLOT ######################################################
+def readtxtFile(path): 
+    energy, microxs = [], []
+
+    with open(path, 'r') as file:
+        file.readline()
+        file.readline()
+
+        for line in file:
+            values = line.split()
+            energy.append(float(values[0]))
+            microxs.append(float(values[1]))
+
+    return np.array(energy), np.log(np.array(microxs))
+
+def plot_cum_norm_histogram():
     print(f"\nPlotting cumulative, normalized fissile production vs. energy...")
 
-    fig, axes = plt.subplots(3, 2, figsize=(15,15)) # sharex='col', sharey='row', 
-
-    # load in thorium and uranium background (n, gamma) cross section, shift to 0.1 and 0.9
+    fig, ax = plt.subplots(figsize=(7,7)) # sharex='col', sharey='row'
     u_path = "./Figures/XSPlot/U238gamma.txt"
-    th_path = "./Figures/XSPlot/Th232gamma.txt"
     u238_energy, u238_mxs = readtxtFile(u_path) # cross sections are returned as log(microxs)
-    th232_energy, th232_mxs = readtxtFile(th_path)
-
     u238_mxs_shifted = (u238_mxs - np.min(u238_mxs)) * 0.8 / (np.max(u238_mxs) - np.min(u238_mxs)) + 0.1
-    th232_mxs_shifted = (th232_mxs - np.min(th232_mxs)) * 0.8 / (np.max(th232_mxs) - np.min(th232_mxs)) + 0.1
+    hcpb_u_ng_df = pd.read_csv('./Figures/data/prism_C_U238_n-gamma_900K.csv') 
 
-    titles = [r"FLiBe-UF$_4$", r"FLiBe-ThF$_4$", r"HCPB-UO$_2$", r"HCPB-ThO$_2$", r"DCLL-UO$_2$", r"DCLL-ThO$_2$",]
-    dfs    = [self.flibe_u_ng_df, self.flibe_th_ng_df, self.hcpb_u_ng_df, self.hcpb_th_ng_df, self.dcll_u_ng_df, self.dcll_th_ng_df,]
+    title, df = r"HCPB-UO$_2$ Prism C", hcpb_u_ng_df
 
-    for i in range(3):
-        for j in range(2):
-            ax = axes[i, j]
-            if i == 2:
-                ax.set_xlabel("Incident neutron energy [eV]")
-            if j == 0:
-                ax.set_ylabel(r"Cumulative fraction of fertile $($n,$\gamma)$ rxns")
-                uranium,  = ax.plot(u238_energy, u238_mxs_shifted, color='gray', linewidth=0.7, alpha=0.4, label=r'U238 (n, $\gamma$)')
-                uranium_leg = ax.legend(handles=[uranium], loc='upper left', edgecolor='gray', frameon=True, framealpha=.75)
-                ax.add_artist(uranium_leg)
-            if j == 1: 
-                thorium,  = ax.plot(th232_energy, th232_mxs_shifted, color='gray', linewidth=0.7, alpha=0.4, label=r'Th232 (n, $\gamma$)')
-                thorium_leg = ax.legend(handles=[thorium], loc='upper left', edgecolor='gray', frameon=True, framealpha=.75)
-                ax.add_artist(thorium_leg)
+    ax.set_xlabel("Incident neutron energy [eV]")
+    ax.set_ylabel(r"Cumulative fraction of fertile $($n,$\gamma)$ rxns")
+    uranium, = ax.plot(u238_energy, u238_mxs_shifted, color='gray', linewidth=0.7, alpha=0.4, label=r'U238 (n, $\gamma$)')
+    uranium_leg = ax.legend(handles=[uranium], loc='upper left', edgecolor='gray', frameon=True, framealpha=.75)
+    ax.add_artist(uranium_leg)
 
-    for ax, df, title in zip(axes.flatten(), dfs, titles):
+    df = df[df["fertile_kg/m3"].isin([15,30,60,90,120,150,250,500,750,999.99])]
+    df_mean = df.groupby("fertile_kg/m3")["mean"].sum().to_frame() # pd.DataFrame(, columns=["MT_fertile","sum"]) # 2-col df, colA = MT_fertile values, colB = sum of 'mean'
+    df_mean.columns = ["sum"]   
+    df = df.merge(df_mean, on="fertile_kg/m3", how="left") # adds sum as column to df
+    df['norm_mean'] = df['mean'] / df['sum']
+    
+    edges = np.unique(np.concatenate([df['energy low [eV]'].values, df['energy high [eV]'].values]))
+    bins  = np.sort(df['energy mid [eV]'].unique())
+    lw=0.5
 
-        # Filter out MT_fertile loadings we want to plot 
-        df = df[df["fertile_kg/m3"].isin([30,60,90,120,150])]
+    print(df['fertile_kg/m3'].unique())
 
-        # Compute sum of 'mean' for each MT_fertile. 
-        df_mean = df.groupby("fertile_kg/m3")["mean"].sum().to_frame() # pd.DataFrame(, columns=["MT_fertile","sum"]) # 2-col df, colA = MT_fertile values, colB = sum of 'mean'
-        df_mean.columns = ["sum"]   
-        df = df.merge(df_mean, on="fertile_kg/m3", how="left") # adds sum as column to df
+    sub   = df[df['fertile_kg/m3'] == 15]
+    label = sub['fertile_kg/m3'].iloc[0]
+    _, _, col15 = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='black', label=fr'{label} kg$/$m$^3$', lw=lw)
 
-        df['norm_mean'] = df['mean'] / df['sum']
+    sub   = df[df['fertile_kg/m3'] == 30]
+    label = sub['fertile_kg/m3'].iloc[0]
+    _, _, col30 = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='red', label=fr'{label} kg$/$m$^3$', lw=lw)
 
-        # df.to_csv('test.csv',index=False)
-        
+    sub   = df[df['fertile_kg/m3'] == 60]
+    label = sub['fertile_kg/m3'].iloc[0]
+    _, _, col60 = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='orange', label=fr'{label} kg$/$m$^3$', lw=lw)
 
-        # Create bin edges from low and high energy boundaries
-        edges = np.unique(np.concatenate([df['energy low [eV]'].values, df['energy high [eV]'].values]))
-        bins  = np.sort(df['energy mid [eV]'].unique())
+    sub   = df[df['fertile_kg/m3'] == 90]
+    label = sub['fertile_kg/m3'].iloc[0]
+    _, _, col90 = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='yellow', label=fr'{label} kg$/$m$^3$',lw=lw)
 
-        sub   = df[df['fertile_kg/m3'] == 30]
-        label = int(round(sub['fertile_kg/m3'].iloc[0], -1))
-        _, _, red = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='#ff1f5b', label=fr'{label} kg$/$m$^3$')
+    sub   = df[df['fertile_kg/m3'] == 120]
+    label = sub['fertile_kg/m3'].iloc[0]
+    _, _, col120 = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='green', label=fr'{label} kg$/$m$^3$',lw=lw)
 
-        sub = df[df['fertile_kg/m3'] == 60]
-        label = int(round(sub['fertile_kg/m3'].iloc[0], -1))
-        _, _, orange= ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='#f48628', label=fr'{label} kg$/$m$^3$')
+    sub   = df[df['fertile_kg/m3'] == 150]
+    label = sub['fertile_kg/m3'].iloc[0]
+    _, _, col150 = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='blue', label=fr'{label} kg$/$m$^3$',lw=lw)
 
-        sub = df[df['fertile_kg/m3'] == 90]
-        label = int(round(sub['fertile_kg/m3'].iloc[0], -1))
-        _, _, green= ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='#04cc6c', label=fr'{label} kg$/$m$^3$')
+    sub   = df[df['fertile_kg/m3'] == 250]
+    label = sub['fertile_kg/m3'].iloc[0]
+    _, _, col250 = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='purple', label=fr'{label} kg$/$m$^3$',lw=lw)
 
-        sub = df[df['fertile_kg/m3'] == 120]
-        label = int(round(sub['fertile_kg/m3'].iloc[0], -1))
-        _, _, blue = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='#0c9edd', label=fr'{label} kg$/$m$^3$')
+    sub   = df[df['fertile_kg/m3'] == 500.0]
+    label = sub['fertile_kg/m3'].iloc[0]
+    _, _, col500 = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='deeppink', label=fr'{label} kg$/$m$^3$',lw=lw)
 
-        sub = df[df['fertile_kg/m3'] == 150]
-        label = int(round(sub['fertile_kg/m3'].iloc[0], -1))
-        _, _, purple = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='#b367bd', label=fr'{label} kg$/$m$^3$')
-        
-        ax.xaxis.set_ticks_position('both')
-        ax.yaxis.set_ticks_position('both')
-        ax.yaxis.set_minor_locator(MultipleLocator(0.05))
-        ax.grid(axis='x', which='major', linestyle='-', linewidth=0.5)
-        ax.grid(axis='y', which='major', linestyle='-', linewidth=0.5)
+    sub   = df[df['fertile_kg/m3'] == 999.99]
+    label = 1000 # sub['fertile_kg/m3'].iloc[0]
+    _, _, col1000 = ax.hist(sub['energy mid [eV]'], bins=bins, weights=sub['norm_mean'], cumulative=True, histtype='step', color='lightpink', label=fr'{label} kg$/$m$^3$',lw=lw)
 
-        ax.set_xscale('log')
-        ax.set_xlim(0.67*1e0, 1.5*1e7)
-        ax.set_ylim(-0.03, 1.03)
-        fig.tight_layout()
 
-        leg = ax.legend(handles=[red[0], orange[0], green[0], blue[0], purple[0]], title=title, fancybox=False, edgecolor='black', frameon=True, framealpha=.75, ncol=1, loc="lower right")
-        leg.get_frame().set_linewidth(0.5) 
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    ax.yaxis.set_minor_locator(MultipleLocator(0.05))
+    ax.grid(axis='x', which='major', linestyle='-', linewidth=0.5)
+    ax.grid(axis='y', which='major', linestyle='-', linewidth=0.5)
 
-    if self.save:
-        plt.savefig(f'./Figures/pdf/fig_cum_norm_histogram_new.pdf', bbox_inches='tight', format='pdf')
-        plt.savefig(f'./Figures/png/fig_cum_norm_histogram_new.png', bbox_inches='tight', format='png')
-        print(f"Exported cumulative normalized histogram plots.")
-    else:
-        print(f"Did not export cumulative normalized histogram plot.")
+    ax.set_xscale('log')
+    ax.set_xlim(0.67*1e0, 1.5*1e7)
+    ax.set_ylim(-0.03, 1.03)
+    fig.tight_layout()
 
-    if self.show: plt.show()
-    plt.close('all')
+    # black red orange green blue purple pink
+    leg = ax.legend(handles=[col15[0],col30[0], col60[0], col90[0], col120[0], col150[0], col250[0], col500[0], col1000[0]], 
+                    title=title, fancybox=False, edgecolor='black', frameon=True, framealpha=.75, ncol=1, loc="lower right")
+    leg.get_frame().set_linewidth(0.5) 
+
+    plt.savefig(f'./Figures/pdf/fig_cum_norm_histogram_prism.pdf', bbox_inches='tight', format='pdf')
+
+plot_cum_norm_histogram()
