2db2ad8263f6c37ffe6fa3d49e9e2bc763c62023
Emma Rose Zoccoli <ezoccoli3@login-phoenix-gnr-3.pace.gatech.edu>
2026-01-28 02:43:30 -0500

    Cluster state: wedge collate/plot/fission, cum-norm 3 subplots, flux A/B/C

--- Python/scratch.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-vf_fertile = 100 / (6.7 * 1000)  # 1 g/cm³ = 1000 kg/m³
-fractions  = np.array([1, vf_fertile])
-fractions /= np.array([1, vf_fertile]).sum()
-vf_flibe_new, vf_fertile_new = fractions
-
-print(vf_flibe_new, vf_fertile_new)
